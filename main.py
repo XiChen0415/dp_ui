42f8e23993ba2fb4854a99e0377301fece8de48b
XiChen0415 <xi@fiture.com>
2020-04-20 14:24:18 +0800

    updage

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 import tkinter.messagebox as mbox
-import os,shutil
+import os, shutil
 import tkinter.filedialog
 from tkinter import StringVar, IntVar
 import cv2
-from PIL import Image, ImageTk,ImageDraw, ImageFont
-import numpy as np 
+from PIL import Image, ImageTk, ImageDraw, ImageFont
+import numpy as np
 import sys
 import json
 
@@ -26,32 +26,32 @@ class MainPage():
         self.MainPage.pack()
         tk.Label(self.MainPage,
                  text="选择你要使用的工具：",
-                 font=("Arial",16),
+                 font=("Arial", 16),
                  fg='red'
                  ).pack()
         tk.Button(self.MainPage,
                   text="数据收集",
                   height=5,
                   width=40,
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.switch_1).pack()
         tk.Button(self.MainPage,
                   text="数据处理",
                   height=5,
                   width=40,
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.switch_2).pack()
         tk.Button(self.MainPage,
                   text="数据检验",
                   height=5,
                   width=40,
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.switch_3).pack()
         tk.Button(self.MainPage,
                   text="退出",
                   height=5,
                   width=40,
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.quit).pack()
 
     def switch_1(self,):
@@ -81,20 +81,20 @@ class DataCollect(tk.Frame):
         tk.Label(self.DataCollect,
                  text='数据收集').pack()
         tk.Button(self.DataCollect,
-                  width=40,height=5,
+                  width=40, height=5,
                   text='设备端存图下载',
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.download_from_device).pack()
         tk.Button(self.DataCollect,
-                  width=40,height=5,
+                  width=40, height=5,
                   text='视频截取',
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.capture_video).pack()
         tk.Label(self.DataCollect, textvariable=self.text).pack()
         tk.Button(self.DataCollect,
-                  width=30,height=5,
+                  width=30, height=5,
                   text='返回上一层',
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.back_to_main).pack()
 
     def back_to_main(self):
@@ -105,35 +105,34 @@ class DataCollect(tk.Frame):
         mbox.showinfo(title="待补全", message="功能待补全")
 
     def capture_video(self):
-        #file_path = tk.filedialog.askdirectory()
-        #if file_path != '':
-        file_path = '/Users/xiaoge/Downloads/video1/'
-        self.text.set("您选择的文件是:"+file_path)
+        file_path = tk.filedialog.askdirectory()
+        if file_path != '':
+            # file_path = '/Users/xiaoge/Downloads/video1/'
+            self.text.set("您选择的文件是:"+file_path)
         files = os.listdir(file_path)
         i = 1
-        #vc = cv2.VideoCapture(file_path+files[0])
+        # vc = cv2.VideoCapture(file_path+files[0])
         for video_file in files:
             if video_file != '.DS_Store':
                 vc = cv2.VideoCapture(file_path+video_file)
                 if vc.isOpened():
-                    rval, frame = vc.read() 
+                    rval, frame = vc.read()
                 else:
                     rval = False
                 timeF = 300
                 f = 1
                 while rval:
                     rval, frame = vc.read()
-                    if (f % timeF == 0)& (frame is not None):
+                    if (f % timeF == 0) & (frame is not None):
                         cv2.imwrite('/Users/xiaoge/Downloads/pic1/'+str(i)+'.jpg', frame)
-                        i =i+1
+                        i = i+1
                     f = f+1
                     cv2.waitKey(1)
                 vc.release()
             tk.Button(self.DataCollect, text='确认开始', command=tk.DISABLED).pack()
-        #else:
-           # self.text.set("您没有选择任何文件！")
-           # btn.destroy()
-                
+        # else:
+        # self.text.set("您没有选择任何文件！")
+        # btn.destroy()
 
 
 class DataProcess(tk.Frame):
@@ -144,18 +143,18 @@ class DataProcess(tk.Frame):
         tk.Label(self.DataProcess,
                  text='数据处理').pack()
         tk.Button(self.DataProcess,
-                  width=40,height=5,
+                  width=40, height=5,
                   text='图片裁剪',
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.CropPicture).pack()
         tk.Button(self.DataProcess,
-                  width=40,height=5,
-                  font=("Arial",16),
+                  width=40, height=5,
+                  font=("Arial", 16),
                   text='图片翻转',
                   command=self.FlipPicture).pack()
         tk.Button(self.DataProcess,
                   width=40, height=5,
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   text='返回上一层',
                   command=self.back_to_main).pack()
 
@@ -167,9 +166,9 @@ class DataProcess(tk.Frame):
         mbox.showinfo(title="待补全", message="功能待补全")
 
     def FlipPicture(self):
-        #mbox.showinfo(title="待补全", message="功能待补全")
+        # mbox.showinfo(title="待补全", message="功能待补全")
         path = "/Users/xiaoge/Downloads/pic/"
-        #self.text.set("您选择的文件是:"+file_path)
+        # self.text.set("您选择的文件是:"+file_path)
 
         def rotate_bound(image, angle):
             (h, w) = image.shape[:2]
@@ -192,16 +191,17 @@ class DataProcess(tk.Frame):
                 if not os.path.isdir(filename):
                     _file = file_path+filename
                     img = cv2.imread(_file)
-                    if not img is None:
+                    if img is not None:
                         img_rotate = rotate_bound(img, angle)
                         save_file_path = save_img_path + filename
                         cv2.imwrite(save_file_path, img_rotate)
-        #if __name__ == '__main__':
+        # if __name__ == '__main__':
         raw_img_path = path
         save_img_path = '/Users/xiaoge/Downloads/fan/'
         shutil.rmtree(save_img_path)
         os.mkdir(save_img_path)
         rotate_image(raw_img_path, save_img_path, 90)
+
 
 class DataValidate(tk.Frame):
     def __init__(self, master, img_lb, filepath):
@@ -215,17 +215,17 @@ class DataValidate(tk.Frame):
         tk.Button(self.DataValidate,
                   width=40,
                   text='人体框任务',
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.Det_validate).pack()
         tk.Button(self.DataValidate,
                   width=40,
                   text='关节点任务',
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.Pose_validate).pack()
         tk.Button(self.DataValidate,
                   width=40,
                   text='返回上一层',
-                  font=("Arial",16),
+                  font=("Arial", 16),
                   command=self.back_to_main).pack()
         self.img_lb.pack()
 
@@ -234,12 +234,12 @@ class DataValidate(tk.Frame):
         MainPage(self.master)
 
     def Det_validate(self):
-        #mbox.showinfo(title="待补全", message="功能待补全")
-        filepath = '/Users/xiaoge/Downloads/Fiture_dataset/Detection.json'
+        # mbox.showinfo(title="待补全", message="功能待补全")
+        filepath = '/Users/xiaoge/Downloads/Fiture_dataset/Det_04.json'
         json_file = open(filepath, 'r')
-        annot = json.loads(json_file.read()) 
+        annot = json.loads(json_file.read())
         # 循环 imgpath = []
-        #print(len(annot['info']))
+        # print(len(annot['info']))
         count = 0
         compare = 'Det'
         pos = []
@@ -247,63 +247,44 @@ class DataValidate(tk.Frame):
             if e['file_name'][0:3] == compare:
                 pos.append(count)
             count += 1
-        #imgpath = '/Users/xiaoge/Downloads/Fiture_dataset/2020-Jan-2/'+annot['images'][i]['file_name']
+
         count_ant = 0
-        for e in annot['annotations']:
-            if count_ant not in pos:
-                e['category_id'] = 2
-            if e['id'] == 1:
-                count_ant += 1
-        print(count_ant)
-        path = ''
         cnt = 0
-        for e in pos:
+        for e in annot['annotations']:
             cnt += 1
             if cnt == 10:
-                path = annot['images'][e]['file_name']
-                window_name = annot['images'][e]['file_name']
-                img = cv2.imread('/Users/xiaoge/Downloads/Fiture_dataset/2020-Jan-2/'+ path)
-                img_id = annot['images'][e]['id']
-                cnt += 1
-                for annt in annot['annotations']:
-                    if (annt["image_id"] == annot['images'][e]['id']) and (annt['category_id'] != 2):
+                for d in annot['images']:
+                    if d['id'] == e['image_id']:
+                        file_name = d['file_name']
+                        window_name = d['file_name']
+                        img = cv2.imread('/Users/xiaoge/Downloads/Fiture_dataset/2020-Jan-2/' + file_name)
+                        cnt += 1
                         p1_x = annt['bbox'][0]
-                        p1_y = annt['bbox'][1] 
+                        p1_y = annt['bbox'][1]
                         p2_x = p1_x + annt['bbox'][2]
                         p2_y = p1_y + annt['bbox'][3]
                         cv2.rectangle(img, (p1_x, p1_y), (p2_x, p2_y), (0, 0, 255), 4)
-                cv2.namedWindow('image_window')
-                img_height = img.shape[0]
-                img_width  = img.shape[1]
-                cv2.putText(img, window_name, (0,img_height-50), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 2)
-                cv2.imshow('image_window', img)
-                cv2.waitKey(0)
+                        cv2.namedWindow('image_window')
+                        img_height = img.shape[0]
+                        img_width = img.shape[1]
+                        cv2.putText(img, window_name, (0, img_height-50), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 2)
+                        cv2.imshow('image_window', img)
+                        cv2.waitKey(0)
                 cv2.destroyAllWindows()
                 cnt = 0
-    '''    
-        print(len(annot['images']))
-        print(len(pos))
-        print(len(annot['annotations']))
-        img = Image.fromarray(img)
-        imgtk = ImageTk.PhotoImage(image=img)
-        self.img_lb.config(image=imgtk)
-        self.img_lb.image = imgtk
-        tk.Bottom(self.DataValidate, image=imgtk).pack()
-    '''
+
     def Pose_validate(self):
         file_path = '/Users/xiaoge/Downloads/Fiture_dataset/Pose.json'
         json_file = open(file_path, 'r')
-        annot = json.loads(json_file.read()) 
+        annot = json.loads(json_file.read())
         cnt = 0
         for e in annot['annolist']:
-            cnt +=1
-            if cnt == 100: 
-                path = str(e['imgidx'])+'.jpg'
-                name = path 
-                img = cv2.imread('/Users/xiaoge/Downloads/Fiture_dataset/2020-Jan-1/'+path)
-                #img = cv2.imread('./testdata/'+path)
+            cnt += 1
+            if cnt == 10:
+                file_name = e['imgdix']+'.jpg'
+                img = cv2.imread('/Users/xiaoge/Downloads/Fiture_dataset/2020-Jan-1/'+file_name)
                 img_height = img.shape[0]
-                img_width  = img.shape[1]
+                img_width = img.shape[1]
                 if img_height == 550 or img_height == 720:
                         circle_size = 4
                         text_size = 0.5
@@ -318,34 +299,26 @@ class DataValidate(tk.Frame):
                 for keypoints in e['annopoints']:
                     x = int(keypoints['x'])
                     y = int(keypoints['y'])
-                    #if  keypoints['is_visible']==1:
-                    pose_list.append((x,y))
+                    # if keypoints['is_visible']==1:
+                    pose_list.append((x, y))
                     cv2.circle(img, (x, y), circle_size, (0, 0, 255), -1)
-                    #cv2.putText(img, str(keypoints['id']), (x+10,y+text_position), cv2.FONT_HERSHEY_COMPLEX, text_size, (0, 0, 255), text_width)
-                
-                pairs = [[0, 1], [1, 2], [3, 4], [4, 5], [2, 6], [3, 6], [6, 7], [7, 8], [7, 9], [9, 10], [10, 11], [7, 12], [12,13], [13, 14]]
+                    # cv2.putText(img, str(keypoints['id']), (x+10,y+text_position), cv2.FONT_HERSHEY_COMPLEX, text_size, (0, 0, 255), text_width)
+
+                pairs = [[0, 1], [1, 2], [3, 4], [4, 5], [2, 6], [3, 6], [6, 7], [7, 8], [7, 9], [9, 10], [10, 11], [7, 12], [12, 13], [13, 14]]
                 for (p1, p2) in pairs:
                     if (pose_list[p1] is not None) & (pose_list[p2] is not None):
-                        cv2.line(img, pose_list[p1], pose_list[p2], (0,0,255), thickness= 2)
-                
+                        cv2.line(img, pose_list[p1], pose_list[p2], (0, 0, 255), thickness=2)
+
                 cv2.namedWindow('image_window')
-                cv2.putText(img, name, (0,img_height-50), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 2)
+                cv2.putText(img, name, (0, img_height-50), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 0, 255), 2)
                 cv2.imshow('image_window', img)
                 if cv2.waitKey(0) == 27:
                     break
                 cv2.destroyAllWindows()
                 cnt = 0
-        
-        
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     MainFrame(root)
     root.mainloop()
-
-            
-
-
-                    
-
